a3c1f0992f8e3a5a707465f4c1a7b141678121e2
Daniel Silva <danielsilva@iMac-de-Daniel.local>
2020-09-21 17:59:53 -0300

    feat: alterado o key da ul para repository.id

--- src/Components/Repositories/index.js
+++ src/Components/Repositories/index.js
@@ -7,8 +7,8 @@ export const Repositories = ({ className, title, repos }) => {
     <div className={`${className}`}>
       <h4>{title}</h4>
       <ul>
-        {repos.map((repo, index) => (
-          <li key={index}>
+        {repos.map((repo) => (
+          <li key={repo.id}>
             <a href={repo.link} target="_blank" rel="noopener noreferrer">
               {repo.name}
             </a>
